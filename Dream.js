fe32216c9fd229ac1359a827113ebf94e16c0b4d
mosalah15 <45065625+mosalah15@users.noreply.github.com>
2018-11-18 18:25:53 +0200

    Add files via upload

---
+++ Dream.js
@@ -0,0 +1,14 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("510562519056646156")
+setInterval(function() {
+channel.send(`spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam `);
+}, 25)
+})
+ 
+ 
+client.login('NTA4MDI1MjU2ODcxNDYwODc0.Ds7Ptg.UtUY60UF937mieeNwb0c5KBJP1o');
