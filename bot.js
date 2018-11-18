aefd461487f166fab484f7b6b4c930b5068fbc89
mosalah15 <45065625+mosalah15@users.noreply.github.com>
2018-11-18 21:05:35 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,11 +4,11 @@ console.log("Scrpit By Dream");
 
 
 client.on("ready", () => {
-let channel =     client.channels.get("510562519056646156")
+let channel =     client.channels.get("513776332614664217")
 setInterval(function() {
 channel.send(`spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam `);
 }, 25)
 })
  
  
-client.login('NTA4MDI1MjU2ODcxNDYwODc0.Ds7Ptg.UtUY60UF937mieeNwb0c5KBJP1o');
+client.login('NTEzNzg5NjA5NjMzNzc1NjE3.DtNI5g.f4h9PgQnICqO_xOM2d2WWF8dnGo');
